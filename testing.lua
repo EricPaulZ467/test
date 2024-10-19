e14777cc69c87b0312b1da5e8e298451066fd389
EricPaulZ467 <paulfernandezeric@gmail.com>
2024-10-19 18:05:39 +0800

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,18 +1,15 @@
-_G.UnitsToFind = "Xeno"
-_G.hookinghook = "https://discord.com/api/webhooks/1297127324067565568/C32CspF4obMUqIdQGtz5dIcoPkJtxxC6ob3DhseDvdcnsGV3TMRPz4YRnOG2522Fj-j2"
-
 
 local plr = game.Players.LocalPlayer
 local dataFolder = plr:WaitForChild("Data")
 local units = dataFolder:FindFirstChild("OwnedUnits").Value
-local search = string.find(units, _G.UnitsToFind)
+local search = string.find(units, getgenv().UnitsToFind)
 local found = nil
 
 if search then
-    warn("Found: ", _G.UnitsToFind)
+    warn("Found: ", getgenv().UnitsToFind)
     found = true
 else
-    warn("Can't find: ", _G.UnitsToFind)
+    warn("Can't find: ", getgenv().UnitsToFind)
     found = false
 end
 local data = {
@@ -22,7 +19,7 @@ local data = {
             ["author"] = { ["name"] = "AWTD Unit Finder by Gosling.GG"},
             ["description"] = 
                 "**Username:** " .. "||" .. plr.Name .. "||" .. "\n" .. 
-                "**Unit target:** " .. _G.UnitsToFind .. "\n" .. 
+                "**Unit target:** " .. getgenv().UnitsToFind .. "\n" .. 
                 "**Status:** " .. tostring(found) .. "\n" ..
                 "**Date:** " .. tostring(os.date("%d/%m/%Y")) .. "\n" ..
                 "**Time:** " .. tostring(os.date("%X")) .. "\n\n" ..
@@ -34,6 +31,6 @@ local data = {
 local newdata = game:GetService("HttpService"):JSONEncode(data)
 local headers = { ["content-type"] = "application/json"}
 local request = http_request or request or HttpPost or syn.request
-local caller = {Url = _G.hookinghook, Body = newdata, Method = "POST", Headers = headers}
+local caller = {Url = getgenv().hookinghook, Body = newdata, Method = "POST", Headers = headers}
 
 request(caller)
